b9dd6fd87ba2c7ceea5522055ff7e8204b7998e7
Raidel <yoshilol0526@gmail.com>
2025-01-25 18:02:49 -0300

    Modified important settings for environment variables
    Update requirements
    
    Added default values to environment variables

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=t$*9rvyo@trljhgqcy6u*-9o+103rs)k6yn1!w+90oj19y@n3"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 # Application definition
 
